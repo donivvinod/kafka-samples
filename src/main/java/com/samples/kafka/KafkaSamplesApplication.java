8198bd5fd271273a5ca0a5aab42e2a994bd4590d
vinodkumarv <vinodkumar.vasudevan@gmail.com>
2018-05-01 18:04:59 +0800

    first commit

---
+++ src/main/java/com/samples/kafka/KafkaSamplesApplication.java
@@ -0,0 +1,12 @@
+package com.samples.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSamplesApplication.class, args);
+	}
+}
